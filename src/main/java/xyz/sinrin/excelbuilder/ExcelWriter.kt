b50dc9c22cbfe7791762e341e0bfe36ea550b515
sinrin-ch <sarie@163.com>
2018-03-02 20:38:05 +0800

    写入的数据类型从原来的Iterable<T>增加了数组Array<T>

--- src/main/java/xyz/sinrin/excelbuilder/ExcelWriter.kt
+++ src/main/java/xyz/sinrin/excelbuilder/ExcelWriter.kt
@@ -2,12 +2,12 @@ package xyz.sinrin.excelbuilder
 
 import org.apache.poi.ss.usermodel.Sheet
 
-class ExcelWriter<in T>(private val clazz: Class<T>,
-                        private val templateSheet: Sheet,
-                        private val propertyNameIndex: Map<Int, String>,
-                        private val firstDataRowIndex: Int,
-                        private val styleToBeUsedRowIndex: Int,
-                        private val dynamicColPredicate: ((T) -> Map<Int, Any?>)?) {
+class ExcelWriter<T>(private val clazz: Class<T>,
+                     private val templateSheet: Sheet,
+                     private val propertyNameIndex: Map<Int, String>,
+                     private val firstDataRowIndex: Int,
+                     private val styleToBeUsedRowIndex: Int,
+                     private val dynamicColPredicate: ((T) -> Map<Int, Any?>)?) {
 
     fun writeSheet(excelDataObjects: Iterable<T>): Sheet {
         val wrapper = ExcelWriterWrapper(clazz)
@@ -17,5 +17,9 @@ class ExcelWriter<in T>(private val clazz: Class<T>,
                 }
         wrapper.writeRows(templateSheet, excelDataObjects, firstDataRowIndex, styleToBeUsedRowIndex)
         return this.templateSheet
+    }
+
+    fun writeSheet(excelDataArray: Array<T>): Sheet {
+        return this.writeSheet(excelDataArray.asIterable())
     }
 }
--- src/test/java/xyz/sinrin/excelbuilder/BuilderTest.java
+++ src/test/java/xyz/sinrin/excelbuilder/BuilderTest.java
@@ -18,35 +18,40 @@ public class BuilderTest {
     public static void main(String[] args) throws IOException {
 
         List<User> list = new ArrayList<User>();
-        list.add(new User(1,"张三", 18));
+        list.add(new User(1, "张三", 18));
         list.add(new User(2, "李四", 19));
         list.add(new User(3, "王五", 20));
 
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet();
-        new ExcelBuilder<User>(User.class,sheet)
+        new ExcelBuilder<User>(User.class, sheet)
 //                .configPropertiesSort("id", "name", "", "age")
                 .configDynamicPropertiesCols(new Function1<User, Map<Integer, ?>>() {
                     @Override
                     public Map<Integer, ?> invoke(User user) {
                         Map<Integer, Object> map = new HashMap<Integer, Object>();
                         map.put(user.getId(), user.getId());
-                        map.put(4,user.getName());
-                        map.put(user.getAge()-13,user.getAge());
+                        map.put(4, user.getName());
+                        map.put(user.getAge() - 13, user.getAge());
                         return map;
                     }
                 })
                 .configDataRowIndex(3)
                 .buildWriter()
-                .writeSheet(list);
-        String curentDir = System.getProperty("user.dir" ) + File.separator + "target";
+                .writeSheet(
+                        new User[]{
+                                new User(1, "张三", 18),
+                                new User(2, "李四", 19),
+                                new User(3, "王五", 20)
+                        });
+        String curentDir = System.getProperty("user.dir") + File.separator + "target";
         File path = new File(curentDir);
-        if(!path.exists()){
+        if (!path.exists()) {
             path.mkdirs();
         }
 
-        File file = new File(curentDir,"hehe.xlsx");
-        if(!file.exists()){
+        File file = new File(curentDir, "hehe.xlsx");
+        if (!file.exists()) {
             file.createNewFile();
         }
 
